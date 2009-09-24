9f17e62e2a2019fe5cf253cf5ab0caf151793503
Alexander Kislitsky <sand8080@gmail.com>
2009-09-24 19:16:55 +0400

    Tariff tests into helixtariff_tests added.

--- src/helixtariff_tests.py
+++ src/helixtariff_tests.py
@@ -7,6 +7,7 @@ from helixtariff.test.server.test_server import ServerTestCase #IGNORE:W0611 @Un
 from helixtariff.test.logic.test_service_type import ServiceTypeTestCase #IGNORE:W0611 @UnusedImport
 from helixtariff.test.logic.test_service_set_descr import ServiceSetDescrTestCase #IGNORE:W0611 @UnusedImport
 from helixtariff.test.logic.test_service_set import ServiceSetTestCase #IGNORE:W0611 @UnusedImport
+from helixtariff.test.logic.test_tariff import TariffTestCase #IGNORE:W0611 @UnusedImport
 
 
 if __name__ == '__main__':
