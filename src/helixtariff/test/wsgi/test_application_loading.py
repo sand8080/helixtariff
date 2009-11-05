b5e767ef04f2c5bbd55807a89d98f5dd8b78b47b
Alexander Kislitsky <sand8080@gmail.com>
2009-11-05 13:14:45 +0300

    Loading test renamed.

--- src/helixtariff/test/wsgi/test_application_loading.py
+++ src/helixtariff/test/wsgi/test_application_loading.py
@@ -1,42 +1,28 @@
 #import cProfile
 
-from random import randint
 from decimal import Decimal
 import unittest
-from datetime import datetime
+import datetime
 from eventlet import api, util, coros
 import cjson
 import random
 
+from helixcore.test.util import random_word, select_random, profile
+
 from helixtariff.test.db_based_test import DbBasedTestCase
 from helixtariff.conf import settings
-from helixtariff.test.wsgi.client import Client, random_word
+from helixtariff.test.wsgi.client import Client
 from helixtariff.wsgi.server import Server
 
 util.wrap_socket_with_coroutine_socket()
 
 api.spawn(Server.run)
 
-def profile(func):
-    def decorated(self, *args, **kwargs):
-        print '%s >>>>' % func.func_name
-        repeats = kwargs['repeats']
-        start = datetime.now()
-        for _ in xrange(repeats):
-            func(self, *args, **kwargs)
-        delta = datetime.now() - start
-        if delta.seconds == 0:
-            print 'repeats: %d, elapsed time %s' % (repeats, delta)
-        else:
-            print 'repeats: %d, elapsed time %s ~ [%s per second]' % (repeats, delta, repeats / delta.seconds)
-        print '%s <<<<' % func.func_name
-    return decorated
-
 
 class ApplicationTestCase(DbBasedTestCase):
     def setUp(self):
         super(ApplicationTestCase, self).setUp()
-        self.cli = Client(settings.server_host, settings.server_port, '%s' % datetime.now(), 'qazwsx')
+        self.cli = Client(settings.server_host, settings.server_port, '%s' % datetime.datetime.now(), 'qazwsx')
 
     def check_status_ok(self, raw_result):
         self.assertEqual('ok', cjson.decode(raw_result)['status'])
@@ -50,9 +36,6 @@ class ApplicationTestCase(DbBasedTestCase):
         self.assertEqual('error', result['status'])
         self.assertEqual('validation', result['category'])
 
-    def select_random(self, collection):
-        min_idx, max_idx = 0, len(collection) - 1
-        return collection[randint(min_idx, max_idx)]
 
     def load_detailed_tariff_data(self, tariffs_names):
         tariffs = {}
@@ -62,20 +45,20 @@ class ApplicationTestCase(DbBasedTestCase):
 
     @profile
     def get_tariff_detailed(self, tariffs_names, repeats=1): #IGNORE:W0613
-        tariff_name = self.select_random(tariffs_names)
+        tariff_name = select_random(tariffs_names)
         return self.cli.get_tariff_detailed(tariff_name)
 
     @profile
     def get_price(self, tariffs_detailed, repeats=1): #IGNORE:W0613
-        tariff_name = self.select_random(tariffs_detailed.keys())
+        tariff_name = select_random(tariffs_detailed.keys())
         service_types_names = tariffs_detailed[tariff_name]['types']
-        return self.cli.get_price(tariff_name, self.select_random(service_types_names))
+        return self.cli.get_price(tariff_name, select_random(service_types_names))
 
     @profile
     def get_wrong_price(self, tariffs_detailed, repeats=1): #IGNORE:W0613
-        tariff_name = self.select_random(tariffs_detailed.keys())
+        tariff_name = select_random(tariffs_detailed.keys())
         service_types_names = tariffs_detailed[tariff_name]['types']
-        return self.cli.get_price(tariff_name, self.select_random(service_types_names) + 'fake')
+        return self.cli.get_price(tariff_name, select_random(service_types_names) + 'fake')
 
     def loader_task(self):
         types = [random_word() for _ in range(random.randint(3, 10))]
