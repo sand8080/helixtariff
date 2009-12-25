1fc510b061a7177b6bad9367667a161576e359d5
Alexander Kislitsky <sand8080@gmail.com>
2009-12-25 15:45:42 +0300

    Https handled in application test.

--- src/helixtariff/test/wsgi/client.py
+++ src/helixtariff/test/wsgi/client.py
@@ -3,7 +3,7 @@ import cjson
 
 
 class Client(ClientApplication):
-    def __init__(self, host, port, login, password, protocol):
+    def __init__(self, host, port, login, password, protocol='http'):
         super(Client, self).__init__(host, port, login, password, protocol=protocol)
 
     def ping(self):
